a5c9d63c2e9a816aa46efcceccdb8616afd4584f
cvpcs <root@cvpcs.org>
2010-12-30 01:08:57 -0600

    added logcats to the logging mechanisms

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,7 +73,7 @@ BOARD_CACHE_FILESYSTEM := auto
 
 # information for hijacking
 BOARD_HIJACK_EXECUTABLES := logwrapper
-#BOARD_HIJACK_LOG_ENABLE := true
+BOARD_HIJACK_LOG_ENABLE := true
 BOARD_HIJACK_UPDATE_BINARY := /preinstall/recovery/update-binary
 BOARD_HIJACK_BOOT_UPDATE_ZIP := /system/etc/tanzanite-boot.zip
 BOARD_HIJACK_RECOVERY_UPDATE_ZIP := /preinstall/recovery/recovery.zip
